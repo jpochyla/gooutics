c33d57932c41f42514b31ae49d0f7249de5f8a32
Jan Pochyla <jpochyla@gmail.com>
2023-01-02 03:11:35 +0100

    run everywhere

--- src/main.rs
+++ src/main.rs
@@ -27,7 +27,7 @@ pub fn main() -> Result<()> {
 
 async fn start_server() {
     let app = app::create_router();
-    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 3000));
     Server::bind(&addr)
         .serve(app.into_make_service())
         .await
